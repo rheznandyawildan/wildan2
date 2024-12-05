c11f682313cac254e0278425200171ed59ade7cb
rheznandyawildan <rheznandyawildhan@gmail.com>
2024-12-05 22:44:27 +0700

    Create tugas1.js

---
+++ tugas1.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('changeColor');
+const text = document.getElementById('text');
+
+button.addEventListener('click', function () {
+    text.style.color = 'blue';
+});
