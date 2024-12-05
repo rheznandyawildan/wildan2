fdf1d0421f67180657ea042b5c263b108e567482
rheznandyawildan <rheznandyawildhan@gmail.com>
2024-12-05 22:46:31 +0700

    Create tugas3.js

---
+++ tugas3.js
@@ -0,0 +1,7 @@
+const inputField = document.getElementById('inputField');
+const displayText = document.getElementById('displayText');
+
+
+inputField.addEventListener('keydown', function (event) {
+    displayText.textContent = `Last key pressed: ${event.key}`;
+});
